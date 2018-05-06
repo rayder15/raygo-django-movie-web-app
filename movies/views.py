f5d3eb2300a455a782b0d8bfd3fd0e316265a36f
rayder15 <rayzzor1@yahoo.com>
2018-05-06 19:05:23 +0000

    Created django movie web app

--- movies/views.py
+++ movies/views.py
@@ -20,10 +20,42 @@ def create(request):
     if request.method == 'POST':
         data = {
             'Name': request.POST.get('name'),
-            'Pictures': [{'url': request.POST.get('url')}],
+            'Pictures': [{'url': request.POST.get('url') or 'http://vollrath.com/ClientCss/images/VollrathImages/No_Image_Available.jpg'}],
             'Rating': int(request.POST.get('rating')),
             'Notes': request.POST.get('notes')
         }
 
-        AT.insert(data)
+        try:
+            response = AT.insert(data)
+    #notify on create
+            messages.success(request, 'New Movie Added: {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request, 'Got an error when trying to create a new movie: {}'.format(e))
+    return redirect('/')
+
+def edit(request, movie_id):
+    if request.method == 'POST':
+        data = {
+            'Name': request.POST.get('name'),
+            'Pictures': [{'url': request.POST.get('url') or 'http://vollrath.com/ClientCss/images/VollrathImages/No_Image_Available.jpg'}],
+            'Rating': int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes')
+
+        }
+        try:
+            response = AT.update(movie_id, data)
+        #notify on update
+            messages.success(request, '{}: updated'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request, 'Got an error when trying to update a movie: {}'.format(e))
+    return redirect('/')
+
+
+def delete(request, movie_id):
+    try:
+        movie_name = AT.get(movie_id)['fields'].get('Name')
+        response = AT.delete(movie_id)
+        messages.warning(request, '{}: Deleted'.format(movie_name))
+    except Exception as e:
+        messages.warning(request, 'Got an error when trying to delete a movie: {}'.format(e))
     return redirect('/')
